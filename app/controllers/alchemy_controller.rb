8f8d8d06f62935c84d98ac40760d30ec4eb3f981
JOEL RAMSEY <joel.d.ramsey@gmail.com>
2015-07-09 19:29:01 -0600

    modified alchemy controller. JSON return now persists in PG!

--- app/controllers/alchemy_controller.rb
+++ app/controllers/alchemy_controller.rb
@@ -10,33 +10,15 @@ class AlchemyController < ApplicationController
   	alchemyapi = AlchemyAPI.new()
     response = alchemyapi.combined('url', params[:q],  {'extract'=>'page-image, title, author, concept' })
     puts JSON.pretty_generate(response)
-    json = JSON.parse(response)
-    
-    
-  end
-
-  def new
-    @user = current_user
-    @document = Document.new
-  end
-  
-  def create
-    @user = current_user
-    @document = current_user.documents.build(document_params)
-    
-    if @document.save(document_params)
-      flash[:notice] = "Input was successfully analyzed and persisted."
-      redirect_to request.referrer
       
+    if 
+      Document.create(result: response)
+      flash[:notice] = "Input was successfully analyzed and persisted."
+      redirect_to alchemy_search_path 
+
     else
-      flash[:alert] = "There was an error analyzing the content. Please try again."
-      redirect_to request.referrer
+      flash[:notice] = "Input was successfully analyzed and persisted."
+      redirect_to alchemy_search_path 
     end
-  end
-  
-  private
-  
-  def item_params
-    params.require(:document).permit(:result)
   end
 end
