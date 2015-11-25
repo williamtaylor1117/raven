e9ed5b83aa5105e045a513e2eb40e3246b99b937
joelramsey <joel.d.ramsey@gmail.com>
2015-11-25 16:57:48 -0700

    changed name and removed elasticsearch code

--- app/controllers/search_controller.rb
+++
@@ -1,8 +0,0 @@
-class SearchController < ApplicationController
-  def index
-  	@search = Document.search( *Document.search_params(params) )
-    #@search = Document.entities.search(*Document.search_params(params))
-    #@search = Document.keywords.search(*Document.search_params(params))
-
-  end
-end
